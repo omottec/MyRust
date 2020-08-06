b1d9f13ff96a97e4041407fd850dd51f0a42d2a1
omottec <omottec@gmail.com>
2020-08-06 22:03:28 +0800

    data_type

---
+++ data_type/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+
+    let a = [1, 2, 3, 4, 5];
+    let index = 10;
+    let element = a[index];
+    println!("The value of element is: {}", element);
+}
